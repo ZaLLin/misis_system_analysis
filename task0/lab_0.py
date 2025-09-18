447d10d0e44c12d66045caa94b045cdfc4290036
ZaLLin <114442397+ZaLLin@users.noreply.github.com>
2025-09-18 11:23:16 +0300

    Update lab_0.py

--- task0/lab_0.py
+++ task0/lab_0.py
@@ -1,19 +1,10 @@
 import pandas as pd
 
 def main(path):
-    graph = pd.read_csv(path, header=None)
-
-    d = {}
-
-    for key, val in graph.values:
-        d.setdefault(key, []).append(val)
-
-    uniq_list = list(set(graph[0]).union(set(graph[1])))
-
+    graph = pd.read_csv(path, header=None, dtype=int)
+    uniq_list = sorted(set(graph[0]).union(set(graph[1])))
     matrix = pd.DataFrame(0, index=uniq_list, columns=uniq_list, dtype=int)
-
-    for u, neighbors in d.items():
-        for v in neighbors:
-            matrix.loc[u, v] = 1
-
+    for u, v in graph.values:
+        matrix.loc[u, v] = 1
+        matrix.loc[v, u] = 1
     return matrix
